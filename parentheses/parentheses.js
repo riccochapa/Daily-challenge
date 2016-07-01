7f8a6065988bbc3eaf51f75f11e84275d22ab67a
Ricco <riccochapa@gmail.com>
2016-06-30 23:05:06 -0500

    Update

--- parentheses/parentheses.js
+++ parentheses/parentheses.js
@@ -1,3 +1,20 @@
-var DataStructures = require('algorithms').DataStructures;
-var Stack = DataStructures.Stack;
-var Queue = DataStructures.Queue;
+var pair = function(input) {
+
+  var ying = { '[' : ']' , '{' : '}' , '(' : ')' };
+  var stack = [];
+
+  for (var i = 0; i < input.length; i ++) {
+    var yang = input[i];
+    if (ying[yang]) {
+      stack.push(yang);
+    } else if (ying[stack.pop()] !== yang) {
+        return false;
+      }
+  }
+  return stack.length === 0;
+};
+
+console.log(pair('{}((((()))))'));
+console.log(pair('[()]{}{[()()]()}'));
+console.log(pair('{}()[]'));
+console.log(pair('{[[](]}][[))))))]]'));
