e399993014cdd20ed2b9c26f79e34205ebb95fb3
Ricco <riccochapa@gmail.com>
2016-07-02 02:14:10 -0500

    Updated
    streamlined code.

--- word_count.js
+++ word_count.js
@@ -1,25 +1,21 @@
 function count(string) {
 
-  string = string.trim().toLowerCase().replace(/[\n\t+]/g, " ").replace(/ +/g, " ");
-  var words = string.split(" ");
-  var wordCount = string.split(" ");
+	string = string.trim().toLowerCase().replace(/[\n\t+]/g, " ").replace(/ +/g, " ").split(" ");
+	var words = string;
+	var word = string;
 
-  var a = new Object();
-  for (var i = 0; i < words.length; i++) {
-    var counter = 0;
-    for (var j = 0; j < wordCount.length; j++) {
+	var a = new Object();
+	for (var i = 0; i < words.length; ++i) {
+		    var counter = false;
+		for (var j = 0; j < word.length; ++j) {
 
-      if (words[i] == wordCount[j]) {
-        counter++;
-        delete wordCount[j];
-
-        if (counter > 0) {
-          a[ [ words[i] ] ] = counter;
-        }
-      }
-    }
-  }
- return(a);
+			if (words[i] == word[j])
+				++counter;
+			if (counter > 0)
+				a[[words[i]]] = counter;
+		}
+	}
+	return (a);
 };
 
 describe('count()', function() {
