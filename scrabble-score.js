ff883503e09d126fadb908da552992b640f7987e
Ricco <riccochapa@gmail.com>
2016-07-03 01:00:11 -0500

    Update

---
+++ scrabble-score.js
@@ -0,0 +1,47 @@
+function score(string) {
+  var total = 0;
+	if (string == null){
+	 return total += 0;
+	}
+  var string = string.toUpperCase();
+for (var i = 0; i <= string.length; i++)
+  switch (string[i]) {
+    case "Q": case "Z": total += 10; break;
+    case "J": case "X": total += 8; break;
+    case "K": total += 5; break;
+    case "F": case "H": case "V": case "W": case "Y": total += 4; break;
+    case "B": case "C": case "M": case "P": total += 3; break;
+    case "D": case "G": total += 2; break;
+    case "A": case "E": case "I": case "O": case "U": case "L": case "N": case "R": case "S": case "T": total += 1; break;
+	 default: total += 0;
+  };
+  return total;
+};
+
+/*Jasmine test
+
+describe('Scrabble', function() {
+  it('scores an empty word as zero',function() {
+    expect(score('')).toEqual(0);
+  });
+
+  it('scores a null as zero',function() {
+    expect(score(null)).toEqual(0);
+  });
+
+  it('scores a very short word',function() {
+    expect(score('a')).toEqual(1);
+  });
+
+  it('scores the word by the number of letters',function() {
+    expect(score('street')).toEqual(6);
+  });
+
+  it('scores more complicated words with more',function() {
+    expect(score('quirky')).toEqual(22);
+  });
+
+  it('scores case insensitive words',function() {
+    expect(score('OXYPHENBUTAZONE')).toEqual(41);
+  });
+});
---
+++ scrabble-score.md
@@ -0,0 +1,42 @@
+/*
+# Scrabble Score
+
+Write a program that, given a word, computes the scrabble score for that word.
+
+## Letter Values
+
+You'll need these:
+
+```plain
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+```
+
+## Examples
+"cabbage" should be scored as worth 14 points:
+
+- 3 points for C
+- 1 point for A, twice
+- 3 points for B, twice
+- 2 points for G
+- 1 point for E
+
+And to total:
+
+- `3 + 2*1 + 2*3 + 2 + 1`
+- = `3 + 2 + 6 + 3`
+- = `5 + 9`
+- = 14
+
+### Instructions
+1. Review the description, and create a score method that returns the score based on the rules above.
+2. Once you have a passing test suite, add your code to your daily-challenges repo
+3. Link us to your scrabble.js file on Slack.
+
+*/
