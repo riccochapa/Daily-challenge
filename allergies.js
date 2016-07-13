04a8c82114adb6af1dbbb21304e1f4de5099ff40
Ricco <riccochapa@gmail.com>
2016-07-12 21:56:32 -0500

    reverted to format before airbnb.
    there must have been an error when converting it to airbnb style guide.
    reverted to get back functioning code.

--- allergies.js
+++ allergies.js
@@ -1,46 +1,114 @@
-const Allergies = function allergies(score) {
-  const selectedAllergens = [];
-  const finalAllergens = [];
+var Allergies = function(score) {
 
-  const allergensAndScore = [
-		['eggs', 1],
-		['peanuts', 2],
-		['shellfish', 4],
-		['strawberries', 8],
-		['tomatoes', 16],
-		['chocolate', 32],
-		['pollen', 64],
-		['cats', 128],
-  ];
+	var selectedAllergens = [];
+	var finalAllergens = [];
+	score = score % 256;
 
-  for (let i = 0; i < allergensAndScore.length; i++) {
-    const grade = allergensAndScore[i][1];
-    if (score >= grade) {
-      selectedAllergens.push(allergensAndScore[i]);
-    } else {
-      break;
-    }
+	var allergensAndScore = [
+		["eggs", 1],
+		["peanuts", 2],
+		["shellfish", 4],
+		["strawberries", 8],
+		["tomatoes", 16],
+		["chocolate", 32],
+		["pollen", 64],
+		["cats", 128],
+	];
 
-    if (selectedAllergens[--i]) {
-      finalAllergens.push(selectedAllergens[i]);
-      let currentScore = score - selectedAllergens[i][1];
-      for (let j = (i - 1); j >= 0; j--) {
-        const nextScore = currentScore - selectedAllergens[j][1];
-        if (nextScore >= 0) {
-          finalAllergens.push(selectedAllergens[j]);
-          currentScore = currentScore - selectedAllergens[j][1];
-        }
-      }
-    }
-  }
+	for (var i = 0; i < allergensAndScore.length; i++) {
+		var allergen = allergensAndScore[i][0];
+		var grade = allergensAndScore[i][1];
+		if (score >= grade) {
+			selectedAllergens.push(allergensAndScore[i]);
+		} else {
+			break;
+		}
+	}
 
-  Allergies.prototype.list = function list() {
-    return (finalAllergens.map(function map() {
-      return this.map(allergensAndScore[0]);
-    }).reverse());
-  };
+	if (selectedAllergens[--i]) {
+		finalAllergens.push(selectedAllergens[i]);
+		var currentScore = score - selectedAllergens[i][1];
+		for (var j = (i - 1); j >= 0; j--) {
+			var nextScore = currentScore - selectedAllergens[j][1];
+			if (nextScore >= 0) {
+				finalAllergens.push(selectedAllergens[j]);
+				currentScore = currentScore - selectedAllergens[j][1];
+			}
+		}
+	}
 
-  Allergies.prototype.allergicTo = function allergicTo(potentialAllergen) {
-    return (this.list().indexOf(potentialAllergen) >= 0);
-  };
+	Allergies.prototype.list = function() {
+		return (finalAllergens.map(function(allergen) {
+			return allergen[0];
+		}).reverse());
+	};
+
+	Allergies.prototype.allergicTo = function(potentialAllergen) {
+		return (this.list().indexOf(potentialAllergen) >= 0);
+	};
 };
+
+describe('Allergies', function() {
+
+	it('no allergies at all', function() {
+		var allergies = new Allergies(0);
+		expect(allergies.list()).toEqual([]);
+	});
+
+	it('allergies to eggs', function() {
+		var allergies = new Allergies(1);
+		expect(allergies.list()).toEqual(['eggs']);
+	});
+
+	it('allergies to peanuts', function() {
+		var allergies = new Allergies(2);
+		expect(allergies.list()).toEqual(['peanuts']);
+	});
+
+	it('allergies to strawberries', function() {
+		var allergies = new Allergies(8);
+		expect(allergies.list()).toEqual(['strawberries']);
+	});
+
+	it('allergies to eggs and peanuts', function() {
+		var allergies = new Allergies(3);
+		expect(allergies.list()).toEqual(['eggs', 'peanuts']);
+	});
+
+	it('allergies to more than eggs but not peanuts', function() {
+		var allergies = new Allergies(5);
+		expect(allergies.list()).toEqual(['eggs', 'shellfish']);
+	});
+
+	it('allergic to lots of stuff', function() {
+		var allergies = new Allergies(248);
+		expect(allergies.list()).toEqual(['strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']);
+	});
+
+	it('allergic to everything', function() {
+		var allergies = new Allergies(255);
+		expect(allergies.list()).toEqual(['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']);
+	});
+
+	it('no allergies means not allergic', function() {
+		var allergies = new Allergies(0);
+		expect(allergies.allergicTo('peanuts')).toEqual(false);
+		expect(allergies.allergicTo('cats')).toEqual(false);
+		expect(allergies.allergicTo('strawberries')).toEqual(false);
+	});
+
+	it('allergic to eggs', function() {
+		var allergies = new Allergies(1);
+		expect(allergies.allergicTo('eggs')).toEqual(true);
+	});
+
+	it('allergic to eggs and other things', function() {
+		var allergies = new Allergies(5);
+		expect(allergies.allergicTo('eggs')).toEqual(true);
+	});
+
+	it('ignore non allergen score parts', function() {
+		var allergies = new Allergies(509);
+		expect(allergies.list()).toEqual(['eggs', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']);
+	});
+});
