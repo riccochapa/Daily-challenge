37fac69d6e8d057e697a7cea6b6ce96527e93bae
Ricco <riccochapa@gmail.com>
2016-07-12 23:03:02 -0500

    update

--- allergies.js
+++ allergies.js
@@ -1,51 +1,48 @@
-var Allergies = function(score) {
+const Allergies = function allergies(score) {
+  const selectedAllergens = [];
+  const finalAllergens = [];
 
-	var selectedAllergens = [];
-	var finalAllergens = [];
-	score = score % 256;
+  const allergensAndScore = [
+		['eggs', 1],
+		['peanuts', 2],
+		['shellfish', 4],
+		['strawberries', 8],
+		['tomatoes', 16],
+		['chocolate', 32],
+		['pollen', 64],
+		['cats', 128],
+  ];
 
-	var allergensAndScore = [
-		["eggs", 1],
-		["peanuts", 2],
-		["shellfish", 4],
-		["strawberries", 8],
-		["tomatoes", 16],
-		["chocolate", 32],
-		["pollen", 64],
-		["cats", 128],
-	];
+  for (let i = 0; i < allergensAndScore.length; i++) {
+    const grade = allergensAndScore[i][1];
+    if (score >= grade) {
+      selectedAllergens.push(allergensAndScore[i]);
+    } else {
+      break;
+    }
 
-	for (var i = 0; i < allergensAndScore.length; i++) {
-		var allergen = allergensAndScore[i][0];
-		var grade = allergensAndScore[i][1];
-		if (score >= grade) {
-			selectedAllergens.push(allergensAndScore[i]);
-		} else {
-			break;
-		}
-	}
+    if (selectedAllergens[--i]) {
+      finalAllergens.push(selectedAllergens[i]);
+      let currentScore = score - selectedAllergens[i][1];
+      for (let j = (i - 1); j >= 0; j--) {
+        const nextScore = currentScore - selectedAllergens[j][1];
+        if (nextScore >= 0) {
+          finalAllergens.push(selectedAllergens[j]);
+          currentScore = currentScore - selectedAllergens[j][1];
+        }
+      }
+    }
+  }
 
-	if (selectedAllergens[--i]) {
-		finalAllergens.push(selectedAllergens[i]);
-		var currentScore = score - selectedAllergens[i][1];
-		for (var j = (i - 1); j >= 0; j--) {
-			var nextScore = currentScore - selectedAllergens[j][1];
-			if (nextScore >= 0) {
-				finalAllergens.push(selectedAllergens[j]);
-				currentScore = currentScore - selectedAllergens[j][1];
-			}
-		}
-	}
+  Allergies.prototype.list = function list() {
+    return (finalAllergens.map(function map() {
+      return this.map(allergensAndScore[0]);
+    }).reverse());
+  };
 
-	Allergies.prototype.list = function() {
-		return (finalAllergens.map(function(allergen) {
-			return allergen[0];
-		}).reverse());
-	};
-
-	Allergies.prototype.allergicTo = function(potentialAllergen) {
-		return (this.list().indexOf(potentialAllergen) >= 0);
-	};
+  Allergies.prototype.allergicTo = function allergicTo(potentialAllergen) {
+    return (this.list().indexOf(potentialAllergen) >= 0);
+  };
 };
 
 describe('Allergies', function() {
